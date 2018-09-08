4552a97b46ae32daa0b10164a24237ae57f1970c
mdevaney <mbdevaney@gmail.com>
2018-09-08 15:05:31 -0500

    added images

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.pokemon_list, name='pokemon_list'),
     path('pokemon_add', views.pokemon_add, name='pokemon_add'),
-    path('pokemon/<int:pk>', views.PokemonDetail.as_view(), name='pokemon_detail'),
+    path('pokemon/<int:pk>/detail', views.PokemonDetail.as_view(), name='pokemon_detail'),
 ]
 
 
